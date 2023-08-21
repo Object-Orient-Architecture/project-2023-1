52ab1769fae72c0eed4d799837f2c18acf56fa14
Mmm <dpemrk7@gmail.com>
2023-08-22 01:13:23 +0900

    venv initialized

--- New_DongHyuk/main/sma_app.py
+++ New_DongHyuk/main/sma_app.py
@@ -1,6 +1,6 @@
 from constants.options import *
 from zipfile import ZipFile
-from main.operator import Operator
+from operator_ import Operator
 
 
 class SMA:
@@ -18,6 +18,7 @@ class SMA:
     def __init__(self):
         self.options = {}
         self.zip_file = None
+        self.operator = Operator()
 
     #1 | Methods
     
@@ -25,13 +26,13 @@ class SMA:
     def set_options(
         self,
         building_type: BuildingType = BuildingType.NATURAL,
-        detailType: DetailType = DetailType.MASS,
+        detail_type: DetailType = DetailType.MASS,
         vegetation_type: VegetationType = VegetationType.POINT,
         contour_type: ContourType = ContourType.CONTOUR,
     ):
         self.options = {
             "building_type": building_type,
-            "detailType": detailType,
+            "detailType": detail_type,
             "vegetation_type": vegetation_type,
             "contour_type": contour_type,
         }
@@ -41,12 +42,15 @@ class SMA:
     
     #1 -2 | Operate
     def operate(self):
-        operator = Operator()
-        operator.set_zip_file(self.zip_file)
-        operator.unzip()
-        operator.find_elements("BUILDING")
-        operator.bake_elements_tojson()
-        operator.remove_unzipped()
+        try:
+            self.operator.set_zip_file(self.zip_file)
+            self.operator.unzip()
+            self.operator.find_elements("BUILDING")
+            self.operator.bake_elements_tojson()
+        except Exception as e:
+            raise e
+        finally:
+            self.operator.remove_unzipped()
         
     #1 -3 | Get Result
     def get_result(self):
