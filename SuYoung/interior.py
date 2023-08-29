94f2384e54ad5d26adb468147f391a5901a65bb0
su0kim <soo0kim@naver.com>
2023-08-29 09:33:55 +0900

    add place method

--- SuYoung/interior.py
+++ SuYoung/interior.py
@@ -108,7 +108,7 @@ class Furniture:
         self.axis.Transform(transform)
         self.axis.Unitize()
 
-    def axis_align(self,target_axis=rg.Vector3d):
+    def align_axis(self,target_axis=rg.Vector3d):
         '''
         목표 축을 입력하면 해당 축에 가구의 축을 정렬하는 함수
         self, target_axis : rg.Vector3D -> None
@@ -146,54 +146,154 @@ class Furniture:
         가구의 점유 영역 (가구 영역 + 통로 900) 을 도출하는 함수
         self -> region : rg.PolylineCurve
         '''
-
         return self.area.Offset(rg.Plane.WorldXY,900,0,rg.CurveOffsetCornerStyle.Sharp)
+    
+    def arrange_to_Wall(self, region):
+        '''
+        영역에 걸쳐 배치된 가구를 영역에 맞춰주는 함수
+        self, room : Room => bool
+        '''
+        corners = [rg.Point3d(pt) for pt in self.area.ToPolyline()]
+        possible = []
         
+        for pt in corners:
+            if (region.Contains(pt,rg.Plane.WorldXY,0)==rg.PointContainment.Inside):
+                possible.append(pt)
+
+        if len(possible) == 1:
+            point = possible[0]
+        elif len(possible) > 1:
+            point = rg.Polyline(possible).PointAt(0.5)
+        else:
+            return False
+            
+        if (point):
+            self.move(point)
+            return True
+    
+    def attatch_to_wall(self,region,point,target_axis):
+        '''
+        영역, 위치, 축을 입력하면 해당 위치와 축에 맞춰 방의 벽에 가구를 붙혀주는 함수
+        self, region : rg.PolylineCurve, point : rg.Point3D, target_axis : rg.Vector3D -> None
+        '''
+        self.move(point)
+        self.align_axis(target_axis)
+        self.arrange_to_Wall(region)
+    
 class Table(Furniture):
     def __init__( self, Model = rg.Brep, Type = str ):
         Furniture.__init__(self,Model)
         self.type = Type
         
     def place(self,room):
-        if (room.type == "침실") and (self.type == "desk"):
-            # 1. 방의 모양에 따라 축을 비교해 가구 배치            
-            # 1-1. 방이 가로형일 때: 축과 90도      
-            if (room.shape==1):
-                point = room.wall[1][0]
-                target_axis = -1 * room.wall[0].Direction                       
+        # 책상일 경우
+        if self.type == "desk":    
+            self.place_desk(room)   
+        
+        # 커피 테이블일 경우
+        elif self.type == "coffe table":
+            self.find_sofa_long(room)
+
+    def place_desk(self, room):
+        '''
+        방에 책상을 배치하는 함수
+        '''
+        # 1. 방의 모양에 따라 축을 비교해 가구 배치            
+        # 1-1. 방이 가로형일 때: 축과 90도  
+        if (room.shape==1):
+            point = room.corner[0]
+            target_axis = -1 * room.wall[0].Direction                       
             
-            # 1-2. 방이 정방,세로형일 때: 축과 180도
-            elif (room.shape==0):
-                point = room.wall[2][1]
-                target_axis = -1 * room.wall[1].Direction
-            
+        # 1-2. 방이 정방,세로형일 때: 축과 180도
+        elif (room.shape==0):
+            point = room.corner[2]
+            target_axis = -1 * room.wall[1].Direction
+        
+        # 2. 배치
+        self.attatch_to_wall(room.region,point,target_axis)
+        
+    def find_sofa_long(self,room):
+        '''
+        방에 배치된 긴 소파를 찾아 자동으로 배치하는 함수
+        '''
+        point,target_axis = (0,0)
+        
+        #1. 책상의 위치를 탐색하여 점과 방향 설정
+        for fur in room.placed_furniture:
+            if (fur.type == "sofa long"):
+                point = fur.position + (fur.axis * (fur.depth/2 + self.depth/2 + 500)) 
+                target_axis = fur.axis * -1
+                
+        #2. 이동 및 축 정렬
+        if point and target_axis:
             self.move(point)
-            self.axis_align(target_axis)
-
-            #2. 벽에 붙도록 위치 조정
-            corners = [rg.Point3d(pt) for pt in self.area.ToPolyline()]
-            for pt in corners:
-                if (room.region.Contains(pt,rg.Plane.WorldXY,0)==rg.PointContainment.Inside):
-                    self.move(pt)         
-        return
-
+            self.align_axis(target_axis)        
+            
 class Chair(Furniture):
     def __init__( self, Model = rg.Brep, Type = str ):
         Furniture.__init__(self,Model)
         self.type = Type
     
     def place(self,room):
-        # 침실에 놓이는 의자일 경우
-        if (room.type == "침실") and (self.type == "chair"):
-            # 책상의 위치를 탐색하여 점과 방향 설정
-            for fur in room.placed_furniture:
-                if (fur.type == "desk"):
-                    point = fur.position + (fur.axis * (fur.depth/2 + self.depth/2)) 
-                    target_axis = fur.axis * -1
-            # 이동 및 축 정렬
-            self.move(point)
-            self.axis_align(target_axis)
+        # 책상용 의자일 경우        
+        if self.type == "chair":
+            self.find_desk(room)
+        
+        # 긴 소파일 경우
+        elif self.type == "sofa long":
+            self.place_sofa_long(room)
+        
+        # 1인용 소파일 경우
+        elif self.type == "sofa":
+            self.find_coffe_table(room)
 
+    def place_sofa_long(self, room):
+        '''
+        방에 긴 소파를 배치하는 함수
+        '''
+        #1. 소파가 놓일 벽을 찾아 중점과 축을 설정
+        target_axis = room.valid_wall[1].Direction
+        point = room.valid_wall[0].PointAt(0.5)
+            
+        #2. 이동
+        self.attatch_to_wall(room.region,point,target_axis)
+        
+    def find_desk(self,room):
+        '''
+        방에 배치된 책상의 위치를 찾아 자동으로 배치하는 함수
+        '''
+        point,target_axis = (0,0)
+        
+        #1. 책상의 위치를 탐색하여 점과 방향 설정
+        for fur in room.placed_furniture:
+            if (fur.type == "desk"):
+                point = fur.position + (fur.axis * (fur.depth/2 + self.depth/2 + 100)) 
+                target_axis = fur.axis * -1
+                
+        #2. 이동 및 축 정렬
+        if point and target_axis:
+            self.move(point)
+            self.align_axis(target_axis)
+            
+    def find_coffe_table(self,room):
+        '''
+        방에 배치된 커피 테이블을 찾아 자동으로 배치하는 함수
+        '''
+        point,target_axis = (0,0)
+        
+        #1. 책상의 위치를 탐색하여 점과 방향 설정
+        for fur in room.placed_furniture:
+            if (fur.type == "coffe table"):
+                axis = fur.axis
+                axis.Rotate(math.pi/2,rg.Vector3d(0,0,1))
+                point = fur.position + (axis * (fur.width/2 + self.depth/2 + 500)) 
+                target_axis = axis * -1
+                
+        #2. 이동 및 축 정렬
+        if point and target_axis:
+            self.move(point)
+            self.align_axis(target_axis)
+        
         
 class Shelf(Furniture):
     def __init__( self, Model = rg.Brep, Type = str ):
@@ -201,7 +301,41 @@ class Shelf(Furniture):
         self.type = Type
     
     def place(self,room):
-        return
+        
+        # 옷장일 경우
+        if (self.type == "closet"):
+            self.place_closet(room)
+            
+        # 긴 (낮은) 선반일 경우
+        elif (self.type == "shelf long"):
+            self.place_shelf_long(room)
+
+    def place_shelf_long(self, room):
+        '''
+        길고 낮은 선반(티비 장)을 배치하는 함수
+        '''
+        #1. 선반이 놓일 벽을 찾아 위치와 축 설정
+        point = room.valid_wall[-1].PointAt(0.5)
+        target_axis = room.valid_wall[1].Direction * (-1)
+        
+        #2. 이동
+        self.attatch_to_wall(room.region,point,target_axis)
+            
+
+    def place_closet(self, room):
+        #1.방의 형상에 따라 가구 위치와 축 조정
+        #1-1. 가로형일때
+        if (room.shape ==1):
+            point = room.corner[2]
+            target_axis = room.wall[-1].Direction
+                
+        #1-2. 세로, 정방형 일때
+        elif (room.shape ==0):
+            point = room.corner[0]
+            target_axis = room.wall[1].Direction
+            
+        #2.이동
+        self.attatch_to_wall(room.region,point,target_axis)
 
 class Bed(Furniture):
     def __init__( self, Model = rg.Brep, Type = str ):
@@ -209,9 +343,8 @@ class Bed(Furniture):
         self.type = Type
     
     def place(self,room):
-        #1. 모서리에 가구 위치
-        point = room.wall[1][1]
-        self.move(point)
+        #1. 모서리에 찾기
+        point = room.corner[1]
 
         #2. 방의 형상에 따라 가구의 축 방향 정렬
         if (room.shape==1):
@@ -219,16 +352,9 @@ class Bed(Furniture):
 
         elif (room.shape==0):
             target_axis = -1 * room.wall[1].Direction
-        
-        self.axis_align(target_axis)
 
-        #3. 벽에 붙도록 위치 조정
-        corners = [rg.Point3d(pt) for pt in self.area.ToPolyline()]
-        for pt in corners:
-            if (room.region.Contains(pt,rg.Plane.WorldXY,0)==rg.PointContainment.Inside):
-                self.move(pt)         
-        return
-        
+        #3. 이동
+        self.attatch_to_wall(room.region,point,target_axis)      
 
 # 방 클래스   
 class Room:
@@ -252,7 +378,7 @@ class Room:
         self -> region : rg.PolylineCurve
         '''
         full_region = self.region
-        if len(self.wall)>3:
+        if self.door.Length<1000:
             door_region = rg.PolylineCurve([self.door[0],self.door[1],self.door[1]+self.axis*self.door.Length,self.door[0]+self.axis*self.door.Length,self.door[0]])
             return rg.PolylineCurve.CreateBooleanDifference(full_region,door_region)[0]
         else:
@@ -391,7 +517,21 @@ class LivingRoom(Room):
         self.type = Type
         self.fur_type_list = ["sofa long","coffe table","shelf long","sofa","shelf"]
         self.furnitures = self.get_furniture_list()
-
+        self.valid_wall = self.set_wall()
+        
+    def set_wall(self):
+        """
+        벽들 중에 연산에 사용할 벽만 반환한다.
+        Returns:
+        self => valid_wall : [rg.Line]
+        """
+        if len(self.wall) == 5 :
+            valid_wall = self.wall[1:-1]
+        elif len(self.wall) == 4:
+            valid_wall = self.wall[1:]
+        else:
+            valid_wall = self.wall
+        return valid_wall
                
 class BedRoom(Room):
     def __init__(self,Plan=rg.Polyline,Type=str):
@@ -421,5 +561,8 @@ def set_room(plan=rg.Polyline,usage=int):
     '''
     room = Room(plan)
     typeroom = room.set_roomtype(usage)
+    if typeroom.depth < 2500 or typeroom.width < 2500:
+        print("방이 너무 작습니다.")
+        return None
     
     return typeroom
