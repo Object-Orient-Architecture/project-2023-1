0b7b2fa0e0b0894f1894492f53a81091125674d0
deanvilgel <deanvilgel21@gmail.com>
2023-09-01 11:32:48 +0900

    Final

--- Junmin/draw_rhino.py
+++ Junmin/draw_rhino.py
@@ -1,95 +1,110 @@
 import rhino3dm as rh
 
 class Model:
+    """
+    라이노 3dm파일을 지정합니다. 
+    """
+
     def __init__(self):
+        # type: () -> None
+
         self.file_dir = 'C:/Users/ERSATZ-CAMPUSTOWN/Documents/GitHub/project-2023-1/Junmin/rhino_md/rhino_temp.3dm'
         self.file3dm = rh.File3dm()
     def build_to_rhino(self):
+        # type: () -> bool
+        """
+        라이노 파일을 작성합니다. 
+        작성 성공시 True를 리턴합니다. 
+        """
+ 
         bool = self.file3dm.Write(self.file_dir, 7)
         return bool
-
+    
 def coords_to_rhino_point(coords):
+    # type: (list) -> rh.Point3d
+    """
+    좌표를 라이노 포인트로 전환합니다. 
+    """
+
     return rh.Point3d(coords[0],coords[1],0)
 
 class InterestPostProcess: 
+    """
+    흥미로운 주제의 리스트를 라이노로 굽는 과정을 정의하는 클래스 입니다. 
+    """
+
     def __init__(self, model: Model,  interest_list:list):
+        # type: (Model, list) -> None
+
         self.interest_list = interest_list
         self.model = model
     
     def build_to_rhino(self, heightdict, heightscale):
+        # type: (dict, int) -> None
+        """
+        높이 데이터(얼마나 특이한지의 지표)와 높이의 배수를 입력하면 모델을 올립니다. 
+        """
+
         for interest in self.interest_list:
             for polygon in interest.polygons:
-
                 height = heightdict[interest.name]*heightscale
-
-
                 coords = polygon.exterior.coords
-
-
                 points = rh.Point3dList([coords_to_rhino_point(coord) for coord in coords])
-
                 poly_line = rh.Curve.CreateControlPointCurve(points,1)
 
+                attr = rh.ObjectAttributes()
+                attr.Name = interest.name
                 Extrusion_up = rh.Extrusion().Create(planarCurve = poly_line, height =  float(height), cap = True)
                 Extrusion_down = rh.Extrusion().Create(planarCurve = poly_line,height = - float(height), cap = True)
-                
-                if (Extrusion_up is not None):
-                    
-                    bbox = Extrusion_up.GetBoundingBox()
 
+                if (Extrusion_up is not None):
+                    bbox = Extrusion_up.GetBoundingBox()
+                    
                     bool = bbox.Center.Z > 0
                     if bool:
-                        self.model.file3dm.Objects.AddExtrusion(Extrusion_up)
+                        self.model.file3dm.Objects.AddExtrusion(Extrusion_up, attr)
+                        
                     else:
-                        self.model.file3dm.Objects.AddExtrusion(Extrusion_down)
-
+                        self.model.file3dm.Objects.AddExtrusion(Extrusion_down, attr)
 class BuildingPostProcess: 
+    """
+    흥미로운 건물의 리스트를 라이노로 굽는 과정을 정의하는 클래스 입니다. 
+    """
+
     def __init__(self, model: Model, building_dict:dict):
+        # type: (Model, dict) -> None
+
         self.building_dict = building_dict
         self.model = model
-    
-    def build_to_rhino(self, heightscale:int, heightdict:dict ):
+
+    def build_to_rhino(self, heightdict:dict, heightscale:int):
+        # type: (dict, int) -> None
+        """
+        높이 데이터(얼마나 특이한지의 지표)와 높이의 배수를 입력하면 건물을 올립니다. 
+        """
+
         def draw_coord_to_extrusion(polygon, height):
             coords = polygon.exterior.coords
-
             points = rh.Point3dList([coords_to_rhino_point(coord) for coord in coords])
-
             poly_line = rh.Curve.CreateControlPointCurve(points,1)
-
             Extrusion_up = rh.Extrusion().Create(planarCurve = poly_line, height =  float(height), cap = True)
             Extrusion_down = rh.Extrusion().Create(planarCurve = poly_line,height = - float(height), cap = True)
-            
             if (Extrusion_up is not None):
-                
                 bbox = Extrusion_up.GetBoundingBox()
-
                 bool = bbox.Center.Z > 0
                 if bool:
                     self.model.file3dm.Objects.AddExtrusion(Extrusion_up)
                 else:
                     self.model.file3dm.Objects.AddExtrusion(Extrusion_down)
-
-
         for name, buildings in self.building_dict.items():
             height = heightdict[name] * heightscale
             for building in buildings:
                 polygon = building.polygon
-
                 new_height= height * building.floor
-
-
                 if polygon.geom_type == 'MultiPolygon':
                     for geom in polygon.geoms:
                         draw_coord_to_extrusion(geom, new_height)
                 elif polygon.geom_type == 'Polygon':
                     draw_coord_to_extrusion(polygon, new_height)
-
-
-
-
-
                 else:
                     pass
-
-
-
--- Junmin/main.py
+++ Junmin/main.py
@@ -10,22 +10,28 @@ your_address = "관천로 121"
 your_geocoder_key = "27E280E5-0E68-3751-85C5-2802D1FA2BD1"
 your_nsdi_key = "88c6b251809f4831a79145d085e07ded"
 your_code_key = "BNoOz%2BvJj4mb3cjDIrHx8%2FTWz0JeUmbBmpUWffNOwXmkUnOc21ivl6ra6OOcKby5wd0LNs9Eq6TzqvP34oZy7A%3D%3D"
-
-# 바운딩 박스의 반경 값입니다.
 your_bound = 350
-class Main:
-    def __init__(self) -> None:
-        pass
 
-    # 주소와 키, 유저가 인풋하는 창구입니다.
-    def get_input_main (self, address: str, geocoder_key: str, nsdi_key: str, code_key: str, bound: int):
+class AbnormalFinder:
+    """
+    좌표를 넣어주면 사방의 비교를 해주는 함수입니다
+    """
+
+    def __init__(self, address: str, geocoder_key: str, nsdi_key: str, code_key: str, bound: int):
+        # type: (str, str, str, str, int) -> None
+
         self.address = address 
         self.geocode_key = geocoder_key
         self.nsdi_key = nsdi_key
         self.code_key = code_key
         self.bound = bound
     
-    def get_four_direction_coord (self):
+    def get_four_direction_list (self):
+        # type: () -> None
+        """
+        주소를 토대로 사방의 흥미로운 지점들을 찾습니다.
+        """
+        
         # 주소로 부터 좌표를 찾습니다.
         self.coordinate_centre = addr.CoordinateFromAddress(self.address, self.geocode_key)
         # 찾은 좌표를 바운딩 박스로 제작합니다.
@@ -38,15 +44,11 @@ class Main:
             self.bbox_centre.south
         ]
 
-    def get_four_direction_bbox (self):
         self.four_direction_bbox = [
             addr.BBoxFromCoordinate(coord[0],coord[1], self.bound) 
             for coord in self.four_direction_coord
         ]
         self.seqjson_centre = self.bbox_centre.get_seq_data(key = self.nsdi_key)
-
-    def get_four_direction_abnormal(self):
-        # seq를 훑으며 특이점을 찾습니다
 
         self.four_direction_abnormal = [
             check.AbnormalCheck(
@@ -58,44 +60,36 @@ class Main:
             for bbox in self.four_direction_bbox
         ]
 
-        for abnormal in self.four_direction_abnormal:
-            
-
-        try:
-            usability_south = self.abnormal_south.use_dict
-        except:
-            usability_south = False
-        try:
-            usability_west = self.abnormal_west.use_dict
-        except:
-            usability_west = False
-        try:
-            usability_east = self.abnormal_east.use_dict
-        except:
-            usability_east = False
-
-
-        interests_north = self.abnormal_north.interest_list
-        interests_south = self.abnormal_south.interest_list
-        interests_west = self.abnormal_west.interest_list
-        interests_east = self.abnormal_east.interest_list
-        
         self.usability_list = []
-        if usability_north:
-            self.usability_list.append(usability_north)
-        if usability_south:
-            self.usability_list.append(usability_south)
-        if usability_east:
-            self.usability_list.append(usability_east)
-        if usability_west:
-            self.usability_list.append(usability_west)         
-        self.interests_list = [interests_north,interests_south,interests_west,interests_east]
+        self.interests_list = []
+
+        for abnormal in self.four_direction_abnormal:
+            try:
+                usability = abnormal.use_dict
+            except:
+                usability = False
+            interests = abnormal.interest_list
+
+        
+            if usability:
+                self.usability_list.append(usability)
+            self.interests_list.append(interests)
+
     
     def compare_four_direction(self):
-        self.dictcomp = cp.Comparison_dict(self.usability_list)
-        self.interecomp = cp.Comparison_interest(self.interests_list)
+        # type: () -> None
+        """
+        전단계의 결과를 토대로 사방의 흥미로운 지점들을 비교합니다.
+        """
+
+        self.dictcomp = cp.DictionaryComparison(self.usability_list)
+        self.interecomp = cp.InterestsComparison(self.interests_list)
 
     def draw_four_direction(self):
+        # type: () -> None
+        """
+        비교 결과를 라이노로 나타냅니다.
+        """
         rhino3d = draw.Model()
         dictcomp = self.dictcomp
         interecomp = self.interecomp
@@ -105,40 +99,23 @@ class Main:
             buildings_drawing = draw.BuildingPostProcess(rhino3d,use_targetdict)
             buildings_drawing.build_to_rhino(heightscale=20, heightdict=use_heightdict)
             print (use_heightdict)
-
         for inter in range(len(self.interests_list)):
             inter_targetdict = interecomp.interest_list[inter]
             inter_heightdict = interecomp.compare_interests(inter)
             interest_drawing = draw.InterestPostProcess(rhino3d,inter_targetdict)
             interest_drawing.build_to_rhino(heightscale=100, heightdict=inter_heightdict)
             print (inter_heightdict)
-            
         done = rhino3d.build_to_rhino()
         if (done):
             print ("비교에 성공했습니다.")
 
-Mainpro = Main()
-Mainpro.get_input_main(address= your_address, geocoder_key= your_geocoder_key, nsdi_key= your_nsdi_key, code_key=your_code_key, bound= your_bound)
-Mainpro.get_four_direction_coord()
-Mainpro.get_four_direction_bbox()
-Mainpro.get_four_direction_abnormal()
-Mainpro.compare_four_direction()
-Mainpro.draw_four_direction()
-
-
-
-
-# rhino 파일로 제작하기
-
-
-
-# plt.show()
-
-
-# ref4에서 보다 싶이 국소적으로 유의미한 정보는 
-# (도로명주소) (연속주제) (건물통합정보-마스터)에 한정됩니다.
-# 연속주제는 그중에서 특수한 정보를 담고 있으므로 따로 특이구역 체크를 진행하며
-# 도로명 주소와 건물 통합 정보-마스터에 있는 건물의 용도와 면적, 도로의 면적 등의 정보를 체크
-# 주소지 근처에 특이 건물의 개수, 주거환경의 차이 등을 확인합니다.
-
-# 88c6b251809f4831a79145d085e07ded
+process = AbnormalFinder(
+    address= your_address, 
+    geocoder_key= your_geocoder_key, 
+    nsdi_key= your_nsdi_key, 
+    code_key=your_code_key, 
+    bound= your_bound
+)
+process.get_four_direction_list()
+process.compare_four_direction()
+process.draw_four_direction()
